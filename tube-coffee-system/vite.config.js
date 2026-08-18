bb4f9b347940e7f666f8e4fbffecbde2e3ec1209
ponnarysem0676-oss <ponnary.sem.0676@happychandara.org>
2026-08-19 00:31:01 +0700

    Update vite.config.js

--- tube-coffee-system/vite.config.js
+++ tube-coffee-system/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/TUBE-COFFEE/",
+  base: "/TUBE-COFFEE-/",
   plugins: [react(), tailwindcss()],
 });
